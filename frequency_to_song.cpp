1db28ae20a02a20f4394f595d92bb5b6df829a1f
AlanEsquenazi <esquenazialan@gmail.com>
2019-07-18 06:35:59 -0400

    bug fixes

--- frequency_to_song.cpp
+++ frequency_to_song.cpp
@@ -5,30 +5,35 @@ using namespace std;
 #define all(a) a.begin(), a.end()
 set <pds> song_list;
 double delta = 5;
+string range_tester = "aaa";
 int my = 0;
-vector <string> last_played = ("00", 20);
+vector <string> last_played = {"00","00","00","00","00","00","00","00","00","00","00","00",
+"00","00","00","00","00","00","00","00"};
 double step_freq;
 /*
 TODO:
 -read frequency from file
 -pretty much all file manipulation
+-write chosen song to file
+-read user's song list from file
+-figure out how to get a song's spotify ID
  */
 void decide_song(double frequency){
     double low = frequency-delta;
     double high = frequency+delta;
-    auto interval_begin = lower_bound(all(song_list),mp(low, 'a'));
-    auto interval_end = upper_bound(all(song_list),mp(high, 'a'));
+    auto interval_begin = lower_bound(all(song_list),mp(low, range_tester));
+    auto interval_end = upper_bound(all(song_list),mp(high, range_tester));
     int best_choice = 0;
     for(auto it = interval_begin;it != interval_end;it++){
-        if(last_played.find((*it))!=last_played.end()){
-            last_played.pop_front();
-            last_played.push_back(*it);
+        if(find(all(last_played),((*it).second))!=last_played.end()){
+            last_played.erase(last_played.begin());
+            last_played.push_back((*it).second);
             best_choice++;
             //here, write song to file
             return;
         }
     }
-    last_played.erase(best_choice);
+    last_played.erase(last_played.begin()+best_choice);
     //here, write song to file
     return;
 }
